f6480323f5a73074ea56bb01026a4188d336a4d0
akhan <akhan.dulatbay@gmail.com>
2024-10-22 09:27:04 +0500

    Initial commit

---
+++ src/main/java/com/example/kzh/KzHApplication.java
@@ -0,0 +1,13 @@
+package com.example.kzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KzHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KzHApplication.class, args);
+    }
+
+}
